2b61ed5fbc5af35036cca467aafa178eb91b155d
Alexander Alvarez <67664433+Alex553-bot@users.noreply.github.com>
2021-04-16 08:46:05 -0400

    Add files via upload

---
+++ Punto.java
@@ -0,0 +1,17 @@
+public class Punto{
+    private double x;
+    private double y;
+    
+    Punto(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    public double getX(){
+        return x;
+    }
+    
+    public double getY(){
+        return y;
+    }
+}
